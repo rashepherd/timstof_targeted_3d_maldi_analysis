2e3acc9e1e0abfbd889b976b630072253297f77c
rashepherd <151571415+rashepherd@users.noreply.github.com>
2024-02-08 10:59:03 -0800

    Update run_batch_map.py

--- bin/run_batch_map.py
+++ bin/run_batch_map.py
@@ -1,11 +1,12 @@
 import os
 import argparse
-import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
 from pyTDFSDK.init_tdf_sdk import init_tdf_sdk_api
 from pyTDFSDK.classes import TdfData
 from pyTDFSDK.tims import tims_scannum_to_oneoverk0, tims_oneoverk0_to_scannum, tims_index_to_mz, tims_read_scans_v2
+
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -34,44 +35,46 @@ def get_args():
                         required=True,
                         type=float)
     parser.add_argument('--IS_mz',
-                        help='Optional: m/z value from the feature list to be used for intensity normalization.',
+                        help='The mz value to be used for intensity normalization.',
                         type=float)
     arguments = parser.parse_args()
     return vars(arguments)
 
+
 def run():
     # Parse arguments
     args = get_args()
-	
-    # Set output directory to default if not specified.	
+
+    # Set output directory to default if not specified.
     if args['outdir'] == '':
         args['outdir'] = os.path.split(args['input'])[0]
-	    
+
     if args['outfile'] == '':
         args['outfile'] = os.path.splitext(os.path.split(args['input'])[-1])[0] + '.csv'
-	    
+
     feature_df = pd.read_csv(args['feature_list'])
-	
+
     # Load TDF data
     dll = init_tdf_sdk_api()
     tdf_data = TdfData(args['input'], dll)
-	
+
     list_of_scan_dicts = []
-	
+
+    IS_mz = args.get('IS_mz')  # Get IS_mz argument value if provided
+
     for index, row in feature_df.iterrows():
         mz = row['mz']
         mz_tol = row['mz_tol']
         ook0 = row['ook0']
         ook0_tol = row['ook0_tol']
-        IS_mz = args['IS_mz'] if 'IS_mz' in args else None
-        IS_tol = row['mz_tol'] if IS_mz is not None else None
-	    
-	#Each frame == one spectrum from a MALDI spot
-	#MaldiFrameInfo table in analysis.tdf SQL database tells which frame is associated with each spot    
+        # Each frame == one spectrum from a MALDI spot
+        # MaldiFrameInfo table in analysis.tdf SQL database tells which frame is associated with each spot
         for frame in range(1, tdf_data.analysis['MaldiFrameInfo'].shape[0] + 1):
             scan_dict = {}
-            frames_dict = tdf_data.analysis['Frames'][tdf_data.analysis['Frames']['Id'] == frame].to_dict(orient='records')[0]
-            maldiframeinfo_dict = tdf_data.analysis['MaldiFrameInfo'][tdf_data.analysis['MaldiFrameInfo']['Frame'] == frame].to_dict(orient='records')[0]
+            frames_dict = tdf_data.analysis['Frames'][tdf_data.analysis['Frames']['Id'] ==
+                                                      frame].to_dict(orient='records')[0]
+            maldiframeinfo_dict = tdf_data.analysis['MaldiFrameInfo'][tdf_data.analysis['MaldiFrameInfo']['Frame'] ==
+                                                                      frame].to_dict(orient='records')[0]
             scan_dict['Frame'] = int(frames_dict['Id'])
             scan_dict['Spot'] = maldiframeinfo_dict['SpotName']
             scan_dict['mz'] = mz
@@ -79,76 +82,90 @@ def run():
             scan_dict['ook0'] = ook0
             scan_dict['ook0_tol'] = ook0_tol
             scan_dict['intensity'] = 0
-            if IS_mz is not None and IS_tol is not None:
-                scan_dict['IS_mz'] = IS_mz
-                scan_dict['IS_tol'] = IS_tol
-                scan_dict['IS_intensity'] = 0
+            if IS_mz is not None:
+                scan_dict['IS_intensity'] = 0  # Initialize IS_intensity if IS_mz is provided
 
-	    #Get 1/K0 values from scan numbers in run
-            ook0_array = tims_scannum_to_oneoverk0(dll, tdf_data.handle, frame, range(0, frames_dict['NumScans']+1))
-	    #Get 1/K0 values within tolerance
+            # Get 1/K0 values from scan numbers in run
+            ook0_array = tims_scannum_to_oneoverk0(dll, tdf_data.handle, frame, range(0, frames_dict['NumScans'] + 1))
+            # Get 1/K0 values within tolerance
             ook0_within_tolerance = [i for i in ook0_array if ook0 + ook0_tol >= i >= ook0 - ook0_tol]
-	    #Get scan numbers for ook0 values within tolerance
-            ook0_within_tolerance_scannums = tims_oneoverk0_to_scannum(dll, tdf_data.handle, frame, ook0_within_tolerance)
+            # Get scan numbers for ook0 values within tolerance
+            ook0_within_tolerance_scannums = tims_oneoverk0_to_scannum(dll,
+                                                                       tdf_data.handle,
+                                                                       frame,
+                                                                       ook0_within_tolerance)
             ook0_within_tolerance_scannums = [int(i) for i in ook0_within_tolerance_scannums]
-		
-	    #Read scans from current frame
-	    #Each frame has N scans where each scan corresponds to a 1/K0 value
-	    #If 'IS_mz' is defined, the scan numbers for each spot and frame will be read for that feature, ignoring ooK0	
-            if IS_mz is not None and IS_tol is not None:
-                list_of_scans = tims_read_scans_v2(dll, tdf_data.handle, frame, 0, frames_dict['NumScans']+1)
+
+            # Read scans from current frame
+            # each frame has N scans where each scan corresponds to a 1/K0 value
+            if IS_mz is not None:
+                list_of_scans = tims_read_scans_v2(dll, tdf_data.handle, frame, 0, frames_dict['NumScans'] + 1)
             else:
-                list_of_scans = tims_read_scans_v2(dll, tdf_data.handle, frame, min(ook0_within_tolerance_scannums), max(ook0_within_tolerance_scannums))
-            for scan_num in range(len(list_of_scans)):
-                if list_of_scans[scan_num][0].size != 0 and list_of_scans[scan_num][1].size != 0 and list_of_scans[scan_num][0].size == list_of_scans[scan_num][1].size:
+                list_of_scans = tims_read_scans_v2(dll,
+                                                   tdf_data.handle,
+                                                   frame,
+                                                   min(ook0_within_tolerance_scannums),
+                                                   max(ook0_within_tolerance_scannums))
+            scan_begin = 0
+            scan_end = max(ook0_within_tolerance_scannums) - min(ook0_within_tolerance_scannums)
+            for scan_num in range(scan_begin, scan_end):
+                if list_of_scans[scan_num][0].size != 0 \
+                        and list_of_scans[scan_num][1].size != 0 \
+                        and list_of_scans[scan_num][0].size == list_of_scans[scan_num][1].size:
                     mz_array = tims_index_to_mz(dll, tdf_data.handle, frame, list_of_scans[scan_num][0]).tolist()
                     intensity_array = list_of_scans[scan_num][1].tolist()
-		    #Get indices of the m/z values within tolerance
-                    indices = [mz_array.index(i) for i in mz_array if mz + mz_tol >= i >= mz - mz_tol]
+                    # Get indices of m/z values within tolerance
+                    indices = [mz_array.index(i) for i in mz_array
+                               if mz + mz_tol >= i >= mz - mz_tol]
+                    # Sum intensities if m/z value was found within tolerance of feature of interest
+                    # this summed intensity is the intensity of mz +/- mz_tol at ook0 +/- ook0_tol
                     scan_dict['intensity'] += sum([intensity_array[i] for i in indices])
-                    if IS_mz is not None and IS_tol is not None:
-                        is_indices = [mz_array.index(i) for i in mz_array if IS_mz + IS_tol >= i >= IS_mz - IS_tol]
-			#This summed intensity is the intensity of of IS_mz +/- IS_tol 
-                        scan_dict['IS_intensity'] += sum([intensity_array[i] for i in is_indices])
+                    if IS_mz is not None:
+                        # Get indices of IS_mz values within tolerance
+                        IS_indices = [mz_array.index(i) for i in mz_array
+                                      if IS_mz + mz_tol >= i >= IS_mz - mz_tol]
+                        # Sum IS intensities if IS_mz value was found within tolerance
+                        scan_dict['IS_intensity'] += sum([intensity_array[i] for i in IS_indices])
             list_of_scan_dicts.append(scan_dict)
 
     results = pd.DataFrame(list_of_scan_dicts)
     results.to_csv(os.path.join(args['outdir'], args['outfile']), index=False)
-	
+
     # Display the resulting DataFrame with the modified columns
     print(results)
-	
+
     # Split the 'Spot' column into two new columns 'index' and 'integer'
     results[['index', 'integer']] = results['Spot'].str.extract('([A-Za-z]+)(\d+)', expand=True)
 
-    # Group by 'Frame', 'Spot', 'index' and 'integer', and calculate sum of intensities for numerator and denominator	
+    # Normalize intensity if IS_mz is provided
+    if IS_mz is not None:
+        results['intensity'] = results['intensity'] / results['IS_intensity']
+
+    # Group by 'Frame', 'Spot', 'index' and 'integer', and calculate sum of intensities for numerator and denominator
     group_columns = ['Frame', 'Spot', 'index', 'integer']
-    if 'IS_mz' in args:
-        results['n_intensity'] = results['intensity'] / results['IS_intensity']
-    results['numerator_intensity'] = results[results['ook0'] == args['numerator_ook0']].groupby(group_columns, as_index=False)['intensity'].transform('sum')
-    results['denominator_intensity'] = results[results['ook0'] == args['denominator_ook0']].groupby(group_columns, as_index=False)['intensity'].transform('sum')
-    grouped_results = results.groupby(group_columns, as_index=False)[['numerator_intensity', 'denominator_intensity']].sum()
+    results['numerator_intensity'] = results[results['ook0'] == args['numerator_ook0']].groupby(
+        group_columns, as_index=False)['intensity'].transform('sum')
+    results['denominator_intensity'] = results[results['ook0'] == args['denominator_ook0']].groupby(
+        group_columns, as_index=False)['intensity'].transform('sum')
 
-    if 'IS_mz' in args:
-        grouped_results['numerator_intensity'] = grouped_results['numerator_intensity'] / grouped_results['IS_intensity']
-        grouped_results['denominator_intensity'] = grouped_results['denominator_intensity'] / grouped_results['IS_intensity']
+    # Calculate the ratio based on 'numerator_intensity' and 'denominator_intensity'
+    results['ratio'] = results['numerator_intensity'] / results['denominator_intensity']
 
-    grouped_results['ratio'] = grouped_results['numerator_intensity'] / grouped_results['denominator_intensity']
-
-    grouped_results.to_csv(os.path.join(args['outdir'], 'modified_outfile.csv'), index=False)
+    # Save the result to a new CSV file
+    results.to_csv(os.path.join(args['outdir'], 'modified_outfile.csv'), index=False)
 
     # Display the resulting DataFrame with the modified columns
-    print(grouped_results)
-	
+    print(results)
+
     # Group by 'index' and 'integer', then aggregate using the mean of 'ratio'
-    heatmap_data = grouped_results[grouped_results['ratio'] != '-'].groupby(['index', 'integer'])['ratio'].mean().unstack()
-	
+    heatmap_data = results[results['ratio'] != '-'].groupby(['index', 'integer'])['ratio'].mean().unstack()
+
     # Convert the data type of 'ratio' to float
     heatmap_data = heatmap_data.astype(float)
-	
+
     # Save the result to a new CSV file
     heatmap_data.to_csv(os.path.join(args['outdir'], 'heatmap_data.csv'), index=False)
-	
+
     # Create a heatmap of the calculated ratios using seaborn
     plt.figure(figsize=(10, 8))
     sns.heatmap(heatmap_data, annot=True, cmap='viridis', fmt=".2f", cbar_kws={'label': 'Ratio'})
@@ -156,6 +173,7 @@ def run():
     plt.xlabel('Integer')
     plt.ylabel('Index')
     plt.show()
+
 
 if __name__ == "__main__":
     run()
