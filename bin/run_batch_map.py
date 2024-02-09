722ae8377cc626216e4365df9018688b290d5c5d
rashepherd <151571415+rashepherd@users.noreply.github.com>
2024-02-09 14:17:56 -0800

    Update run_batch_map.py

--- bin/run_batch_map.py
+++ bin/run_batch_map.py
@@ -152,17 +152,19 @@ def run():
         results['n_denominator_intensity'] = results['denominator_intensity']
 
     # Group by 'Frame', 'Spot', 'index' and 'integer', and calculate sum of intensities for numerator and denominator
-    group_columns = ['Frame', 'Spot', 'index', 'integer']
     grouped_results = results.groupby(group_columns, as_index=False)[['n_numerator_intensity', 'n_denominator_intensity']].sum()
+
+    # Conditionally define the ratio column based on internal standard normalization
+    if args['IS_mz'] is not None:
+        grouped_results['ratio'] = grouped_results['n_numerator_intensity'] / grouped_results['n_denominator_intensity']
+    else:
+        grouped_results['ratio'] = grouped_results['numerator_intensity'] / grouped_results['denominator_intensity']
 
     # Save the result to a new CSV file
     grouped_results.to_csv(os.path.join(args['outdir'], 'modified_outfile.csv'), index=False)
 
     # Display the resulting DataFrame with the modified columns
     print(grouped_results)
-
-    # Calculate the ratio based on 'n_numerator_intensity' and 'n_denominator_intensity'
-    grouped_results['ratio'] = grouped_results['n_numerator_intensity'] / grouped_results['n_denominator_intensity']
 
     # Group by 'index' and 'integer', then aggregate using the mean of 'ratio'
     heatmap_data = grouped_results[grouped_results['ratio'] != '-'].groupby(['index', 'integer'])['ratio'].mean().unstack()
@@ -183,4 +185,3 @@ def run():
 
 if __name__ == "__main__":
     run()
-
